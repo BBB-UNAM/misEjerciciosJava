8e1de77325c4fe01575660631a47dd0ef42c31a4
Alberto Picasso <picassoa12@gmail.com>
2020-07-22 23:30:53 -0500

    Añadimos ejemplo de declaración de constantes y valores literales

---
+++ valoresLiteralesAndConstantesExample.java
@@ -0,0 +1,19 @@
+public class valoresLiteralesAndConstantesExample{
+
+	//declaracion de constantes en java
+	public static final float pi = 3.1416F;
+	public static final int dia_lunes = 1;
+	public static final int dia_mares = 2;
+	public static final int dia_miercoles = 3;
+	public static final int dia_jueves = 4;
+	public static final int dia_viernes = 5;
+	public static final int dia_sabado = 6;
+	public static final int dia_domingo = 7;
+
+	//Declaracion de variables literales en java
+	int c = 34;
+	String a = "Hola";
+	boolean bandera = false;
+	char b = '%';
+	double h = 3.1416d;
+}
